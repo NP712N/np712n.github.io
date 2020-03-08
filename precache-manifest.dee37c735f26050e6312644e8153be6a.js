a198cc49bb90da1dbcd790b824b0047e0f4b0851
NP712N <npn19998@gmail.com>
2020-03-08 17:59:17 +0700

    Updates

---
+++ precache-manifest.dee37c735f26050e6312644e8153be6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ee3775a9d25c9431211059a5a0d493",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1c5c907fe1829e777f4",
+    "url": "/static/css/main.120b3e72.chunk.css"
+  },
+  {
+    "revision": "785e01f8d574ac40e75b",
+    "url": "/static/js/2.abe1f6a9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.abe1f6a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1c5c907fe1829e777f4",
+    "url": "/static/js/main.ce6adbca.chunk.js"
+  },
+  {
+    "revision": "4cf2b42daa9b5fe1d6ee",
+    "url": "/static/js/runtime-main.921b883f.js"
+  }
+]);
